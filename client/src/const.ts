5adc54f91e60ccf3bc31d581a1ffd54e6e4e4834
gravixrdp <gravixrdp@gmail.com>
2026-06-27 09:37:43 +0530

    Fix frontend error: handle undefined oauth env vars in getLoginUrl

--- client/src/const.ts
+++ client/src/const.ts
@@ -7,6 +7,11 @@ export const getLoginUrl = () => {
   const redirectUri = `${window.location.origin}/api/oauth/callback`;
   const state = btoa(redirectUri);
 
+  // If env vars aren't set, just return # for now
+  if (!oauthPortalUrl || !appId) {
+    return "#";
+  }
+
   const url = new URL(`${oauthPortalUrl}/app-auth`);
   url.searchParams.set("appId", appId);
   url.searchParams.set("redirectUri", redirectUri);
